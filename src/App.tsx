dfa76a0333933f27be7a2a45c930764d7c5bf523
Ilia Lazarev <crusadergo@yandex.ru>
2021-04-26 14:35:53 +0200

    Add simple file upload logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
+import axios from 'axios';
+import React,{ChangeEvent, Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import UploadFile from './components/upload_file';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<UploadFile />
+		)
+	}
 }
 
 export default App;
---
+++ src/components/upload_file.tsx
@@ -0,0 +1,49 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const UploadFile = () => {
+    const [fileSelected, setFileSelected] = React.useState<File>() // also tried <string | Blob>
+
+    const handleFileChange = function (e: React.ChangeEvent<HTMLInputElement>) {
+        const fileList = e.target.files;
+
+        if (!fileList) return;
+
+        setFileSelected(fileList[0]);
+    };
+
+    const upload = function (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
+        if (fileSelected) {
+            const formData = new FormData();
+            formData.append("csv", fileSelected, fileSelected.name);
+
+            axios.post("http://localhost:2000/upload_file", formData, {
+                headers: {"Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+                    'enctype': 'multipart/form-data',
+                    "Content-Type": "multipart/form-data"
+                }
+            });
+
+        }
+    };
+
+    return (
+        <div>
+            <h1>
+                CSV Upload
+            </h1>
+            <h3>
+                File Upload using React!
+            </h3>
+            <div>
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={upload}>
+                    Upload!
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default UploadFile
